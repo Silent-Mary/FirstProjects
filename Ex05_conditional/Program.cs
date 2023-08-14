656995ca0db9353a854f2c19bc562ee5879aebf7
SilentMary <playinglassbead@gmail.com>
2023-08-14 08:17:42 +0100

    added welcoming programme

---
+++ Ex05_conditional/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mary")
+{
+    Console.WriteLine("Hooray, it's MARY!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
