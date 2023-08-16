47e224dfce2e25d5547fd5dc339f05d64feae713
SilentMary <playinglassbead@gmail.com>
2023-08-16 09:55:26 +0100

    aaand the weights

---
+++ WeightsTask/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 1;
+int b = 2;
+int c = 6, d = 8, e = 4;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.WriteLine(max);
